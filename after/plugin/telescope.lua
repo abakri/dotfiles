4a11e1acbc22feb49733c180f378e23e4cab8098
Aslan <aslan.bakri@gmail.com>
2023-03-06 16:28:25 -0500

    Improve telescope UI, show hidden files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -7,6 +7,41 @@ vim.keymap.set("n", "<leader>pv", builtin.git_files, {})
 vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
 
 require("telescope").setup({
+    pickers = {
+        find_files = {
+            hidden = true,
+            follow = true,
+            layout_strategy = "horizontal",
+            theme = "dropdown",
+            layout_config = {
+                width = 0.75,
+                height = 0.75,
+                preview_width = 0.5,
+            },
+        },
+        git_files = {
+            hidden = true,
+            follow = true,
+            layout_strategy = "horizontal",
+            theme = "dropdown",
+            layout_config = {
+                width = 0.75,
+                height = 0.75,
+                preview_width = 0.5,
+            },
+        },
+        live_grep = {
+            hidden = true,
+            follow = true,
+            layout_strategy = "horizontal",
+            theme = "dropdown",
+            layout_config = {
+                width = 0.75,
+                height = 0.75,
+                preview_width = 0.5,
+            },
+        },
+    },
     defaults = {
         mappings = {
             i = {
