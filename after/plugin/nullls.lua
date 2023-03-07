7eefe8874a38953451c0ed644543e731c9dcf8f1
Aslan <aslan.bakri@gmail.com>
2023-03-07 11:35:39 -0500

    Add mypy and ruff

--- after/plugin/nullls.lua
+++ after/plugin/nullls.lua
@@ -1,6 +1,10 @@
-require("null-ls").setup({
+local null_ls = require("null-ls")
+
+null_ls.setup({
     sources = {
-        require("null-ls").builtins.formatting.black,             -- python code formatting
-        require("null-ls").builtins.formatting.isort,             -- python import sorting
+        null_ls.builtins.formatting.black, -- python code formatting
+        null_ls.builtins.formatting.isort, -- python import sorting
+        null_ls.builtins.diagnostics.mypy, -- python type checking
+        null_ls.builtins.diagnostics.ruff, -- python linting
     }
 })
