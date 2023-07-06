e1dd9f1594872a9b754472c34d5a3081f0667cd7
Aslan Bakri <aslan.bakri@gmail.com>
2023-07-06 16:22:36 -0400

    Add nvim surround

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -18,6 +18,18 @@ return {
         }
     },
 
+    -- nvim surround
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
+
     -- tmux integration
     "christoomey/vim-tmux-navigator",
     {
